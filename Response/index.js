ae33a0593235e6bd7d384085a9af0e1f7bb047e9
Shawn OY <shawn.oy@z-tron.net>
2018-10-25 16:36:35 +0800

    Add common responses

--- Response/index.js
+++ Response/index.js
@@ -30,8 +30,24 @@ class Response {
     return this._buildResponse (400, body, isCors)
   }
 
+  unauthorized (body, isCors = false) {
+    return this._buildResponse (401, body, isCors)
+  }
+
+  forbidden (body, isCors = false) {
+    return this.buildResponse (403, body, isCors)
+  }
+
   notFound (body, isCors = false) {
     return this._buildResponse (404, body, isCors)
+  }
+
+  methodNotAllowed (body, isCors = false) {
+    return this._buildResponse (405, body, icCors)
+  }
+
+  tooManyRequests (body, isCors = false) {
+    return this._buildResponse (429, body, isCors)
   }
 
   internalError (body, isCors = false) {
